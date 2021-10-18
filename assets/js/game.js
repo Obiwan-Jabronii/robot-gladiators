585828652a1c9782b1cb2a202766d27d4cc0e27f
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2021-10-18 16:31:41 -0400

    fixed name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,8 +2,16 @@ var randomNumber = function(min, max) {
     var value = Math.floor(Math.random() * (max - min + 1) + min);
     return value;
 };
+var getPlayerName = function() {
+    var name = "";
+    while (name === "" || name === null) {
+        name = prompt("What is your robots name?");
+    }
+    console.log("Your robot's name is " + name);
+    return name;
+};
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -45,7 +53,6 @@ var enemyInfo = [
             attack: randomNumber(10, 14)
         }
 ];
-debugger
 console.log(playerInfo.name, playerInfo.health, playerInfo.attack, playerInfo.money)
 
 // Game States
@@ -95,7 +102,6 @@ var fight = function(enemy) {
 };
 var startGame = function(){
     playerInfo.reset();
-
 for (var i = 0; i < enemyInfo.length; i++) {
     if (playerInfo.health > 0) {
        
