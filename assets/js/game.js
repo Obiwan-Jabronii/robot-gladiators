c66f71e879fcd9489800d5a6c350d8d38f440bea
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2021-10-18 17:09:24 -0400

    added integers to the shop

--- assets/js/game.js
+++ assets/js/game.js
@@ -157,19 +157,17 @@ for (var i = 0; i < enemyInfo.length; i++) {
  }
  var shop= function(){
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the shop? PLease enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the shop? PLease enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     )
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
             break;
         default:
